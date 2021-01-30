4e721d87131df2e15cf63fa9b0c380256e152618
DaaniiM <danimellado1993@gmail.com>
2021-01-30 13:06:57 +0100

    Actualizando 1

--- puntoTest.ts
+++ puntoTest.ts
@@ -9,25 +9,37 @@ let punto1 = new Punto(4,5);
 
 console.log(punto1.toString());
 
+console.log(punto1.getX());
+
+console.log(punto1.getY());
+
+punto1.setX(5);
+
+punto1.setY(4);
+
+console.log(punto1.toString())
+
 console.log("------------");
 
-console.log(punto1.distanciaAlOrigen());
+let puntoA = new Punto(4,5);
+
+console.log(puntoA.distanciaAlOrigen());
 
 console.log("------------");
 
 let punto2 = new Punto(6,8);
 
-console.log(punto1.calcularDistancia(punto2));
+console.log(puntoA.calcularDistancia(punto2));
 
 console.log("------------");
 
-console.log(punto1.calcularCuadrante());
+console.log(puntoA.calcularCuadrante());
 
 console.log("------------");
 
 let puntosAll = [new Punto(5,4),new Punto(6,8),new Punto(7,4),new Punto(5,5),new Punto(9,9)];
 
-console.log(punto1.calcularMasCercano(puntosAll));
+console.log(puntoA.calcularMasCercano(puntosAll));
 
 console.log("------------");
 
